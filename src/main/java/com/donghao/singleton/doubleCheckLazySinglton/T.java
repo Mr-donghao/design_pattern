87a702fd306dc184592c82e818f64484934242ad
bighard <bighard2016@outlook.com>
2018-09-20 00:11:29 +0800

    懒汉式单例模式 解决线程安全问题的三种方法

---
+++ src/main/java/com/donghao/singleton/doubleCheckLazySinglton/T.java
@@ -0,0 +1,11 @@
+package com.donghao.singleton.doubleCheckLazySinglton;
+
+
+public class T implements Runnable {
+
+      @Override
+    public void run() {
+        LazySingleton lazySingleton = LazySingleton.getInstance();
+        System.out.println(Thread.currentThread().getName()+" "+lazySingleton);
+    }
+}
---
+++ src/main/java/com/donghao/singleton/synchronizedClassLazySingleton/LazySingleton.java
@@ -0,0 +1,23 @@
+package com.donghao.singleton.synchronizedClassLazySingleton;
+
+public class LazySingleton {
+
+    private static LazySingleton lazySingleton = null;
+    private LazySingleton(){
+
+    }
+
+
+    public synchronized static LazySingleton getInstance(){
+        if(lazySingleton==null){
+            lazySingleton = new LazySingleton();
+        }
+        return lazySingleton;
+    }
+}
+/**
+ * 1.在getInstance方法上加上synchronized,使该方法变为同步方法。此时可以认为只有一个线程能进入该类
+ *     注：该关键字加在static方法上，则锁加在该类的class文件上
+ *         加在非静态文件上，锁的是该类在内存中的一个对象
+ *
+ */
